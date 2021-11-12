dfbe2d09a30cf8deb7e83eaf0e0157fcdad908d6
Luvai <luvaihassanali@gmail.com>
2021-11-11 23:33:44 -0500

    fix task mgr bug

--- DellHardwareMonitor/Properties/AssemblyInfo.cs
+++ DellHardwareMonitor/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DellHardwareMonitor")]
+[assembly: AssemblyTitle("Dell Hardware Monitor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
